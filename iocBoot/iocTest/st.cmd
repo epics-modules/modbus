da3da2fe5acdf6a1f6e5a37b2fc031d6a0b489b5
Mark Rivers <MarkRivers@users.noreply.github.com>
2007-03-20 19:34:10 +0000

    Deleted epicsThreadSleep; changed syntax of drvModbusTCPAsynConfigure

--- iocBoot/iocTest/st.cmd
+++ iocBoot/iocTest/st.cmd
@@ -4,18 +4,15 @@ dbLoadDatabase("../../dbd/modbusTCP.dbd")
 modbusTCP_registerRecordDeviceDriver(pdbbase)
 
 drvAsynIPPortConfigure("Koyo1","164.54.160.158:502",0,0,1)
-# NOTE: The following epicsThreadSleep is needed or we get errors when connecting to this server below.
-# TRACK THIS DOWN!!!
-epicsThreadSleep(0.2)
 
 # drvModbusTCPAsynConfigure(
 # "Port name"
 # "TCP/IP port name"
-# " PLC type"iocshArgString};
 # "Modbus function code" (1, 2, 3 ...)
 # "Modbus start address"
 # "Modbus length"
 # "Poll time (msec)" (non-zero poll time on output functions means read once at initialization)
+# "PLC type"};
 
 # NOTE: We use octal numbers for the start address and length (leading zeros)
 #       to be consistent with the PLC nomenclature.  This is optional, decimal
@@ -23,40 +20,40 @@ epicsThreadSleep(0.2)
 
 # The DL205 has bit access to the Xn inputs at Modbus offset 4000 (octal)
 # Read 32 bits (X0-X37).  Function code=2.
-drvModbusTCPAsynConfigure("K1_Xn_Bit",      "Koyo1", "Koyo", 2, 04000,  040,  100)
+drvModbusTCPAsynConfigure("K1_Xn_Bit",      "Koyo1", 2,  04000, 040,  100, "Koyo")
 
 # The DL205 has word access to the Xn inputs at Modbus offset 40400 (octal)
 # Read 8 words (128 bits).  Function code=3.
-drvModbusTCPAsynConfigure("K1_Xn_Word",     "Koyo1", "Koyo", 3, 040400, 010,  100)
+drvModbusTCPAsynConfigure("K1_Xn_Word",     "Koyo1", 3, 040400, 010,  100, "Koyo")
 
 # The DL205 has bit access to the Yn outputs at Modbus offset 4000 (octal)
 # Read 32 bits (Y0-Y37).  Function code=1.
-drvModbusTCPAsynConfigure("K1_Yn_In_Bit",   "Koyo1", "Koyo", 1, 04000,  040,  100)
+drvModbusTCPAsynConfigure("K1_Yn_In_Bit",   "Koyo1", 1,  04000, 040,  100, "Koyo")
 
 # The DL205 has bit access to the Yn outputs at Modbus offset 4000 (octal)
 # Write 32 bits (Y0-Y37).  Function code=5.
-drvModbusTCPAsynConfigure("K1_Yn_Out_Bit",  "Koyo1", "Koyo", 5, 04000,  040,  1)
+drvModbusTCPAsynConfigure("K1_Yn_Out_Bit",  "Koyo1", 5,  04000, 040,    1, "Koyo")
 
 # The DL205 has word access to the Yn outputs at Modbus offset 40500 (octal)
 # Read 8 words (128 bits).  Function code=3.
-drvModbusTCPAsynConfigure("K1_Yn_In_Word",  "Koyo1", "Koyo", 3, 040500, 010,  100)
+drvModbusTCPAsynConfigure("K1_Yn_In_Word",  "Koyo1", 3, 040500, 010,  100, "Koyo")
 
 # Write 8 words (128 bits).  Function code=6.
-drvModbusTCPAsynConfigure("K1_Yn_Out_Word", "Koyo1", "Koyo", 6, 040500, 010,  100)
+drvModbusTCPAsynConfigure("K1_Yn_Out_Word", "Koyo1", 6, 040500, 010,  100, "Koyo")
 
 # The DL205 has bit access to the Cn bits at Modbus offset 6000 (octal)
 # Access 256 bits (C0-C377) as inputs.  Function code 1.
-drvModbusTCPAsynConfigure("K1_Cn_In_Bit",   "Koyo1", "Koyo", 1, 06000,  0400, 100)
+drvModbusTCPAsynConfigure("K1_Cn_In_Bit",   "Koyo1", 1,  06000, 0400, 100, "Koyo")
 
 # Acces the same 256 bits (C0-C377) as outputs.  Function code 5.
-drvModbusTCPAsynConfigure("K1_Cn_Out_Bit",  "Koyo1", "Koyo", 5, 06000,  0400, 1)
+drvModbusTCPAsynConfigure("K1_Cn_Out_Bit",  "Koyo1", 5,  06000, 0400,   1, "Koyo")
 
 # The DL205 has word access to the Cn bits at Modbus offset 40600 (octal)
 # We use the first 16 words (C0-C377) as inputs (256 bits)
-drvModbusTCPAsynConfigure("K1_Cn_In_Word",  "Koyo1", "Koyo", 3, 040600, 020,  100)
+drvModbusTCPAsynConfigure("K1_Cn_In_Word",  "Koyo1", 3, 040600, 020,  100, "Koyo")
 
 # We access the same 16 words (C0-C377) as outputs (256 bits)
-drvModbusTCPAsynConfigure("K1_Cn_Out_Word", "Koyo1", "Koyo", 6, 040600, 020,  1)
+drvModbusTCPAsynConfigure("K1_Cn_Out_Word", "Koyo1", 6, 040600, 020,    1, "Koyo")
 
 # Hex trace format on TCP server
 asynSetTraceIOMask("Koyo1",0,4)
