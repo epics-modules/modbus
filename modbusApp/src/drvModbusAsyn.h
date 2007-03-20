bb4b719609aeb1c1815b0e43a02280deaef2e26f
Mark Rivers <MarkRivers@users.noreply.github.com>
2007-03-20 19:49:56 +0000

    Added new commands, put Configure prototype in this file

--- modbusApp/src/drvModbusAsyn.h
+++ modbusApp/src/drvModbusAsyn.h
@@ -3,15 +3,30 @@
     Author: Mark Rivers
     4-Mar-2007
 
-    This is device support for Modbus TCP asyn drivers.
+    These are the public definitions for drvModbusTCPAsyn.
   
 */
 
-/* These are the strings that device support passes to drivers via the asynDrvUser interface.
- * Drivers must return a value in pasynUser->reason that is unique for that command.
+/* These are the strings that device support passes to drivers via 
+ * the asynDrvUser interface.
+ * Drivers must return a value in pasynUser->reason that is unique 
+ * for that command.
  */
 
-#define MODBUS_DATA_COMMAND_STRING            "DATA"            /* int32, write (default) */
-#define MODBUS_ENABLE_HISTOGRAM_COMMAND_STRING "ENABLE_HISTOGRAM" /* UInt32D, write/read */
-#define MODBUS_READ_HISTOGRAM_COMMAND_STRING  "READ_HISTOGRAM"  /* int32Array, read */
+#define MODBUS_DATA_COMMAND_STRING             "MODBUS_DATA" 
+#define MODBUS_ENABLE_HISTOGRAM_COMMAND_STRING "ENABLE_HISTOGRAM"
+#define MODBUS_READ_HISTOGRAM_COMMAND_STRING   "READ_HISTOGRAM"
+#define MODBUS_POLL_DELAY_COMMAND_STRING       "POLL_DELAY"
+#define MODBUS_READ_OK_COMMAND_STRING          "READ_OK"
+#define MODBUS_WRITE_OK_COMMAND_STRING         "WRITE_OK"
+#define MODBUS_IO_ERRORS_COMMAND_STRING        "IO_ERRORS"
+#define MODBUS_LAST_IO_TIME_COMMAND_STRING     "LAST_IO_TIME"
+#define MODBUS_MAX_IO_TIME_COMMAND_STRING      "MAX_IO_TIME"
 
+int drvModbusTCPAsynConfigure(char *portName, 
+                              char *tcpPortName, 
+                              int modbusFunction, 
+                              int modbusStartAddress, 
+                              int modbusLength,
+                              int pollMsec, 
+                              char *plcType);
