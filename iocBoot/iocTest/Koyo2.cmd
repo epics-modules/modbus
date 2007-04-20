834a9fdffe3a4148cd52a8fa8bf13403f1deedfd
Mark Rivers <MarkRivers@users.noreply.github.com>
2007-04-20 16:59:14 +0000

    Removed TCP dependencies in commands and comments

--- iocBoot/iocTest/Koyo2.cmd
+++ iocBoot/iocTest/Koyo2.cmd
@@ -1,8 +1,8 @@
-# st.cmd for modbusTCP
+# st.cmd for modbus
 < envPaths
 
-dbLoadDatabase("../../dbd/modbusTCP.dbd")
-modbusTCP_registerRecordDeviceDriver(pdbbase)
+dbLoadDatabase("../../dbd/modbus.dbd")
+modbus_registerRecordDeviceDriver(pdbbase)
 
 drvAsynIPPortConfigure("Koyo2","164.54.160.200:502",0,1,1)
 modbusInterposeConfig("Koyo2",0,0)
@@ -14,31 +14,31 @@ modbusInterposeConfig("Koyo2",0,0)
 
 # The DL205 has bit access to the Yn outputs at Modbus offset 4000 (octal)
 # Read 32 bits (Y0-Y37).  Function code=1.
-drvModbusTCPAsynConfigure("K2_Yn_In_Bit",       "Koyo2",    1,  04000,  040,    0,  100,    "Koyo")
+drvModbusAsynConfigure("K2_Yn_In_Bit",       "Koyo2",    1,  04000,  040,    0,  100,    "Koyo")
 
 # The DL205 has bit access to the Yn outputs at Modbus offset 4000 (octal)
 # Write 32 bits (Y0-Y37).  Function code=5.
-drvModbusTCPAsynConfigure("K2_Yn_Out_Bit",      "Koyo2",    5,  04000,  040,    0,  1,      "Koyo")
+drvModbusAsynConfigure("K2_Yn_Out_Bit",      "Koyo2",    5,  04000,  040,    0,  1,      "Koyo")
 
 # The DL205 has bit access to the Cn bits at Modbus offset 6000 (octal)
 # Access 256 bits (C0-C377) as inputs.  Function code=1.
-drvModbusTCPAsynConfigure("K2_Cn_In_Bit",       "Koyo2",    1,  06000,  0400,   0,  100,    "Koyo")
+drvModbusAsynConfigure("K2_Cn_In_Bit",       "Koyo2",    1,  06000,  0400,   0,  100,    "Koyo")
 
 # Access the same 256 bits (C0-C377) as outputs.  Function code=5.
-drvModbusTCPAsynConfigure("K2_Cn_Out_Bit",      "Koyo2",    5,  06000,  0400,   0,  1,      "Koyo")
+drvModbusAsynConfigure("K2_Cn_Out_Bit",      "Koyo2",    5,  06000,  0400,   0,  1,      "Koyo")
 
 # The DL205 has word access to the V3000 memory at Modbus offset 3000 (octal)
 # Access 32 words (V3000-V3040) as inputs.  Function code=3, data type signed BCD.
-drvModbusTCPAsynConfigure("K2_V3000_In_Word",   "Koyo2",    3,  03000,  040,    3,  100,    "Koyo")
+drvModbusAsynConfigure("K2_V3000_In_Word",   "Koyo2",    3,  03000,  040,    3,  100,    "Koyo")
 
-# Hex trace format on TCP server
+# Hex trace format on octet server
 asynSetTraceIOMask("Koyo2",0,4)
-# Turn asynTraceIODriver on TCP server
+# Turn asynTraceIODriver on octet server
 #asynSetTraceMask("Koyo2",0,9)
 
-# Hex trace format on modbusTCP server
+# Hex trace format on modbus server
 asynSetTraceIOMask("K2_V3000_In_Word",0,4)
-# Turn on all debugging on TCP server
+# Turn on all debugging on modbus server
 #asynSetTraceMask("K2_V3000_In_Word",0,255)
 
 dbLoadTemplate("Koyo2.substitutions")
