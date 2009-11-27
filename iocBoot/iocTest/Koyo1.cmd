ef06fcd0ce61247acfa69d5463afab9fb4929885
Mark Rivers <MarkRivers@users.noreply.github.com>
2009-11-27 17:12:37 +0000

    Move slave address from interpose layer to modbus packet layer, allowing multiple slave address per asyn IP or serial port

--- iocBoot/iocTest/Koyo1.cmd
+++ iocBoot/iocTest/Koyo1.cmd
@@ -11,10 +11,9 @@ modbus_registerRecordDeviceDriver(pdbbase)
 #                       int noProcessEos);
 drvAsynIPPortConfigure("Koyo1","164.54.160.158:502",0,1,1)
 #modbusInterposeConfig(const char *portName, 
-#                      int slaveAddress, 
 #                      modbusLinkType linkType,
 #                      int timeoutMsec)
-modbusInterposeConfig("Koyo1",0,0,5000)
+modbusInterposeConfig("Koyo1",0,5000)
 
 # Use the following commands for serial RTU or ASCII
 #drvAsynSerialPortConfigure(const char *portName, 
@@ -29,59 +28,60 @@ modbusInterposeConfig("Koyo1",0,0,5000)
 #asynSetOption("Koyo1",0,"stop","1")
 
 # Use the following command for serial RTU
-#modbusInterposeConfig("Koyo1",1,1,1000)
+#modbusInterposeConfig("Koyo1",1,1000)
 
 # Use the following commands for serial ASCII
 #asynOctetSetOutputEos("Koyo1",0,"\r\n")
 #asynOctetSetInputEos("Koyo1",0,"\r\n")
-#modbusInterposeConfig("Koyo1",1,2,1000)
+#modbusInterposeConfig("Koyo1",2,1000)
 
 # NOTE: We use octal numbers for the start address and length (leading zeros)
 #       to be consistent with the PLC nomenclature.  This is optional, decimal
 #       numbers (no leading zero) or hex numbers can also be used.
+#       In these examples we are using slave address 0 (number after "Koyo1").
 
 # The DL205 has bit access to the Xn inputs at Modbus offset 4000 (octal)
 # Read 32 bits (X0-X37).  Function code=2.
-drvModbusAsynConfigure("K1_Xn_Bit",      "Koyo1", 2,  04000, 040,    0,  100, "Koyo")
+drvModbusAsynConfigure("K1_Xn_Bit",      "Koyo1", 0, 2,  04000, 040,    0,  100, "Koyo")
 
 # The DL205 has word access to the Xn inputs at Modbus offset 40400 (octal)
 # Read 8 words (128 bits).  Function code=3.
-drvModbusAsynConfigure("K1_Xn_Word",     "Koyo1", 3, 040400, 010,    0,  100, "Koyo")
+drvModbusAsynConfigure("K1_Xn_Word",     "Koyo1", 0, 3, 040400, 010,    0,  100, "Koyo")
 
 # The DL205 has bit access to the Yn outputs at Modbus offset 4000 (octal)
 # Read 32 bits (Y0-Y37).  Function code=1.
-drvModbusAsynConfigure("K1_Yn_In_Bit",   "Koyo1", 1,  04000, 040,    0,  100, "Koyo")
+drvModbusAsynConfigure("K1_Yn_In_Bit",   "Koyo1", 0, 1,  04000, 040,    0,  100, "Koyo")
 
 # The DL205 has bit access to the Yn outputs at Modbus offset 4000 (octal)
 # Write 32 bits (Y0-Y37).  Function code=5.
-drvModbusAsynConfigure("K1_Yn_Out_Bit",  "Koyo1", 5,  04000, 040,    0,  1, "Koyo")
+drvModbusAsynConfigure("K1_Yn_Out_Bit",  "Koyo1", 0, 5,  04000, 040,    0,  1, "Koyo")
 
 # The DL205 has word access to the Yn outputs at Modbus offset 40500 (octal)
 # Read 8 words (128 bits).  Function code=3.
-drvModbusAsynConfigure("K1_Yn_In_Word",  "Koyo1", 3, 040500, 010,    0,  100, "Koyo")
+drvModbusAsynConfigure("K1_Yn_In_Word",  "Koyo1", 0, 3, 040500, 010,    0,  100, "Koyo")
 
 # Write 8 words (128 bits).  Function code=6.
-drvModbusAsynConfigure("K1_Yn_Out_Word", "Koyo1", 6, 040500, 010,    0,  100, "Koyo")
+drvModbusAsynConfigure("K1_Yn_Out_Word", "Koyo1", 0, 6, 040500, 010,    0,  100, "Koyo")
 
 # The DL205 has bit access to the Cn bits at Modbus offset 6000 (octal)
 # Access 256 bits (C0-C377) as inputs.  Function code=1.
-drvModbusAsynConfigure("K1_Cn_In_Bit",   "Koyo1", 1,  06000, 0400,   0,  100, "Koyo")
+drvModbusAsynConfigure("K1_Cn_In_Bit",   "Koyo1", 0, 1,  06000, 0400,   0,  100, "Koyo")
 
 # Access the same 256 bits (C0-C377) as outputs.  Function code=5.
-drvModbusAsynConfigure("K1_Cn_Out_Bit",  "Koyo1", 5,  06000, 0400,   0,  1,  "Koyo")
+drvModbusAsynConfigure("K1_Cn_Out_Bit",  "Koyo1", 0, 5,  06000, 0400,   0,  1,  "Koyo")
 
 # Access the same 256 bits (C0-C377) as array outputs.  Function code=15.
-drvModbusAsynConfigure("K1_Cn_Out_Bit_Array",  "Koyo1", 15,  06000, 0400,   0,   1, "Koyo")
+drvModbusAsynConfigure("K1_Cn_Out_Bit_Array",  "Koyo1", 0, 15,  06000, 0400,   0,   1, "Koyo")
 
 # The DL205 has word access to the Cn bits at Modbus offset 40600 (octal)
 # We use the first 16 words (C0-C377) as inputs (256 bits).  Function code=3.
-drvModbusAsynConfigure("K1_Cn_In_Word",  "Koyo1", 3, 040600, 020,    0,  100, "Koyo")
+drvModbusAsynConfigure("K1_Cn_In_Word",  "Koyo1", 0, 3, 040600, 020,    0,  100, "Koyo")
 
 # We access the same 16 words (C0-C377) as outputs (256 bits). Function code=6.
-drvModbusAsynConfigure("K1_Cn_Out_Word", "Koyo1", 6, 040600, 020,    0,  1,  "Koyo")
+drvModbusAsynConfigure("K1_Cn_Out_Word", "Koyo1", 0, 6, 040600, 020,    0,  1,  "Koyo")
 
 # We access the same 16 words (C0-C377) as array outputs (256 bits). Function code=16.
-drvModbusAsynConfigure("K1_Cn_Out_Word_Array", "Koyo1", 16, 040600, 020,    0,   1, "Koyo")
+drvModbusAsynConfigure("K1_Cn_Out_Word_Array", "Koyo1", 0, 16, 040600, 020,    0,   1, "Koyo")
 
 # Enable ASYN_TRACEIO_HEX on octet server
 asynSetTraceIOMask("Koyo1",0,4)
--- iocBoot/iocTest/Koyo2.cmd
+++ iocBoot/iocTest/Koyo2.cmd
@@ -11,10 +11,9 @@ modbus_registerRecordDeviceDriver(pdbbase)
 #                       int noProcessEos);
 drvAsynIPPortConfigure("Koyo2","164.54.160.158:502",0,1,1)
 #modbusInterposeConfig(const char *portName,
-#                      int slaveAddress,
 #                      modbusLinkType linkType,
 #                      int timeoutMsec)
-modbusInterposeConfig("Koyo2",0,0,2000)
+modbusInterposeConfig("Koyo2",0,2000)
 
 # Use the following commands for serial RTU or ASCII
 #drvAsynSerialPortConfigure(const char *portName,
@@ -29,35 +28,36 @@ modbusInterposeConfig("Koyo2",0,0,2000)
 #asynSetOption("Koyo2",0,"stop","1")
 
 # Use the following command for serial RTU
-#modbusInterposeConfig("Koyo2",1,1,2000)
+#modbusInterposeConfig("Koyo2",1,2000)
 
 # Use the following commands for serial ASCII
 #asynOctetSetOutputEos("Koyo2",0,"\r\n")
 #asynOctetSetInputEos("Koyo2",0,"\r\n")
-#modbusInterposeConfig("Koyo2",1,2,2000)
+#modbusInterposeConfig("Koyo2",2,2000)
 
 # NOTE: We use octal numbers for the start address and length (leading zeros)
 #       to be consistent with the PLC nomenclature.  This is optional, decimal
 #       numbers (no leading zero) or hex numbers can also be used.
+#       In these examples we are using slave address 0 (number after "Koyo2").
 
 # The DL205 has bit access to the Yn outputs at Modbus offset 4000 (octal)
 # Read 32 bits (Y0-Y37).  Function code=1.
-drvModbusAsynConfigure("K2_Yn_In_Bit",       "Koyo2",    1,  04000,  040,    0,  100,    "Koyo")
+drvModbusAsynConfigure("K2_Yn_In_Bit",       "Koyo2",    0, 1,  04000,  040,    0,  100,    "Koyo")
 
 # The DL205 has bit access to the Yn outputs at Modbus offset 4000 (octal)
 # Write 32 bits (Y0-Y37).  Function code=5.
-drvModbusAsynConfigure("K2_Yn_Out_Bit",      "Koyo2",    5,  04000,  040,    0,  1,      "Koyo")
+drvModbusAsynConfigure("K2_Yn_Out_Bit",      "Koyo2",    0, 5,  04000,  040,    0,  1,      "Koyo")
 
 # The DL205 has bit access to the Cn bits at Modbus offset 6000 (octal)
 # Access 256 bits (C0-C377) as inputs.  Function code=1.
-drvModbusAsynConfigure("K2_Cn_In_Bit",       "Koyo2",    1,  06000,  0400,   0,  100,    "Koyo")
+drvModbusAsynConfigure("K2_Cn_In_Bit",       "Koyo2",    0, 1,  06000,  0400,   0,  100,    "Koyo")
 
 # Access the same 256 bits (C0-C377) as outputs.  Function code=5.
-drvModbusAsynConfigure("K2_Cn_Out_Bit",      "Koyo2",    5,  06000,  0400,   0,  1,      "Koyo")
+drvModbusAsynConfigure("K2_Cn_Out_Bit",      "Koyo2",    0, 5,  06000,  0400,   0,  1,      "Koyo")
 
 # The DL205 has word access to the V3000 memory at Modbus offset 3000 (octal)
 # Access 32 words (V3000-V3040) as inputs.  Function code=3, data type signed BCD.
-drvModbusAsynConfigure("K2_V3000_In_Word",   "Koyo2",    3,  03000,  040,    3,  100,    "Koyo")
+drvModbusAsynConfigure("K2_V3000_In_Word",   "Koyo2",    0, 3,  03000,  040,    3,  100,    "Koyo")
 
 # Enable ASYN_TRACEIO_HEX on octet server
 asynSetTraceIOMask("Koyo2",0,4)
