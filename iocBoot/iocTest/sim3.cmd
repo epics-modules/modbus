b2b91cf77fc70581557ea13cfd259044fb5abaa5
Mark Rivers <MarkRivers@users.noreply.github.com>
2013-05-27 18:16:01 +0000

    New files to test testModbusSyncIO

---
+++ iocBoot/iocTest/sim3.cmd
@@ -0,0 +1,45 @@
+# sim3.cmd
+
+<envPaths
+
+dbLoadDatabase("../../dbd/modbus.dbd")
+modbus_registerRecordDeviceDriver(pdbbase)
+
+# Use the following commands for TCP/IP
+#drvAsynIPPortConfigure(const char *portName,
+#                       const char *hostInfo,
+#                       unsigned int priority,
+#                       int noAutoConnect,
+#                       int noProcessEos);
+drvAsynIPPortConfigure("sim1","164.54.160.31:502",0,0,1)
+#modbusInterposeConfig(const char *portName,
+#                      modbusLinkType linkType,
+#                      int timeoutMsec, 
+#                      int writeDelayMsec)
+modbusInterposeConfig("sim1",0,2000,0)
+
+# Word access at Modbus address 0
+# Access 1 words as inputs.  
+# Function code=3
+# default data type unsigned integer.
+# drvModbusAsynConfigure("portName", "tcpPortName", slaveAddress, modbusFunction, modbusStartAddress, modbusLength, dataType, pollMsec, "plcType")
+drvModbusAsynConfigure("A0_In_Word", "sim1", 0, 3, 0, 4, 0, 100, "Simulator")
+
+# Access 1 words as outputs.  
+# Either function code=6 (single register) or 16 (multiple registers) can be used, but 16
+# is better because it is "atomic" when writing values longer than 16-bits.
+# Default data type unsigned integer.
+# drvModbusAsynConfigure("portName", "tcpPortName", slaveAddress, modbusFunction, modbusStartAddress, modbusLength, dataType, pollMsec, "plcType")
+drvModbusAsynConfigure("A0_Out_Word", "sim1", 0, 6, 0, 4, 0, 1, "Simulator")
+
+# Enable ASYN_TRACEIO_HEX on octet server
+asynSetTraceIOMask("sim1",0,4)
+# Enable ASYN_TRACE_ERROR and ASYN_TRACEIO_DRIVER on octet server
+#asynSetTraceMask("sim1",0,9)
+
+testModbusSyncIOConfigure("TEST_SYNCIO", "A0_In_Word", "A0_Out_Word")
+
+dbLoadTemplate("sim3.substitutions")
+
+iocInit
+
