42d72f7b9ca5792c85a46aac984f207cd48a7615
Mark Rivers <rivers@cars.uchicago.edu>
2016-02-04 17:05:12 -0600

    New file to test asynInt32Array interface

---
+++ iocBoot/iocTest/array_test.cmd
@@ -0,0 +1,52 @@
+< envPaths
+
+dbLoadDatabase("../../dbd/modbus.dbd")
+modbus_registerRecordDeviceDriver(pdbbase)
+
+# Use the following commands for TCP/IP
+#drvAsynIPPortConfigure(const char *portName,
+#                       const char *hostInfo,
+#                       unsigned int priority,
+#                       int noAutoConnect,
+#                       int noProcessEos);
+drvAsynIPPortConfigure("sim1","164.54.160.31:502",0,0,1)
+#modbusInterposeConfig(const char *portName,
+#                      modbusLinkType linkType,
+#                      int timeoutMsec, 
+#                      int writeDelayMsec)
+modbusInterposeConfig("sim1",0,2000,0)
+
+# Word access at Modbus address 0
+# Access 20 words as UINT16 data type.  
+# Function code=3
+# drvModbusAsynConfigure("portName", "tcpPortName", slaveAddress, modbusFunction, modbusStartAddress, modbusLength, dataType, pollMsec, "plcType")
+drvModbusAsynConfigure("UINT16",   "sim1", 0, 3,  0, 20, 0, 100, "Simulator")
+
+# Word access at Modbus address 20
+# Access 20 words as INT16 data type.  
+# Function code=3
+# drvModbusAsynConfigure("portName", "tcpPortName", slaveAddress, modbusFunction, modbusStartAddress, modbusLength, dataType, pollMsec, "plcType")
+drvModbusAsynConfigure("INT16",    "sim1", 0, 3, 20, 20, 4, 100, "Simulator")
+
+# Word access at Modbus address 40
+# Access 20 words as INT32_LE data type.  
+# Function code=3
+# drvModbusAsynConfigure("portName", "tcpPortName", slaveAddress, modbusFunction, modbusStartAddress, modbusLength, dataType, pollMsec, "plcType")
+drvModbusAsynConfigure("INT32_LE", "sim1", 0, 3, 40, 20, 5, 100, "Simulator")
+
+# Word access at Modbus address 60
+# Access 20 words as INT32_BE data type.  
+# Function code=3
+# drvModbusAsynConfigure("portName", "tcpPortName", slaveAddress, modbusFunction, modbusStartAddress, modbusLength, dataType, pollMsec, "plcType")
+drvModbusAsynConfigure("INT32_BE", "sim1", 0, 3, 60, 20, 6, 100, "Simulator")
+
+
+# Enable ASYN_TRACEIO_HEX on octet server
+asynSetTraceIOMask("sim1",0,4)
+# Enable ASYN_TRACE_ERROR and ASYN_TRACEIO_DRIVER on octet server
+#asynSetTraceMask("sim1",0,9)
+
+dbLoadTemplate("array_test.substitutions")
+
+iocInit
+
