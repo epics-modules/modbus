4fdd29fc92a47f249c13bad65df90a0aeba6bdd6
Mark Rivers <rivers@cars.uchicago.edu>
2016-02-08 14:49:12 -0600

    Added output waveform records and changes to test using OFFSET on both input and output waveforms

--- iocBoot/iocTest/array_test.cmd
+++ iocBoot/iocTest/array_test.cmd
@@ -18,27 +18,27 @@ modbusInterposeConfig("sim1",0,2000,0)
 
 # Word access at Modbus address 0
 # Access 20 words as UINT16 data type.  
-# Function code=3
 # drvModbusAsynConfigure("portName", "tcpPortName", slaveAddress, modbusFunction, modbusStartAddress, modbusLength, dataType, pollMsec, "plcType")
-drvModbusAsynConfigure("UINT16",   "sim1", 0, 3,  0, 20, 0, 100, "Simulator")
+drvModbusAsynConfigure("UINT16_F3",   "sim1", 0, 3,  0, 20, 0, 100, "Simulator")
+drvModbusAsynConfigure("UINT16_F16",  "sim1", 0,16,  0, 20, 0, 100, "Simulator")
 
 # Word access at Modbus address 20
 # Access 20 words as INT16 data type.  
-# Function code=3
 # drvModbusAsynConfigure("portName", "tcpPortName", slaveAddress, modbusFunction, modbusStartAddress, modbusLength, dataType, pollMsec, "plcType")
-drvModbusAsynConfigure("INT16",    "sim1", 0, 3, 20, 20, 4, 100, "Simulator")
+drvModbusAsynConfigure("INT16_F3",    "sim1", 0, 3, 20, 20, 4, 100, "Simulator")
+drvModbusAsynConfigure("INT16_F16",   "sim1", 0,16, 20, 20, 4, 100, "Simulator")
 
 # Word access at Modbus address 40
 # Access 20 words as INT32_LE data type.  
-# Function code=3
 # drvModbusAsynConfigure("portName", "tcpPortName", slaveAddress, modbusFunction, modbusStartAddress, modbusLength, dataType, pollMsec, "plcType")
-drvModbusAsynConfigure("INT32_LE", "sim1", 0, 3, 40, 20, 5, 100, "Simulator")
+drvModbusAsynConfigure("INT32_LE_F3", "sim1", 0, 3, 40, 20, 5, 100, "Simulator")
+drvModbusAsynConfigure("INT32_LE_F16","sim1", 0,16, 40, 20, 5, 100, "Simulator")
 
 # Word access at Modbus address 60
 # Access 20 words as INT32_BE data type.  
-# Function code=3
 # drvModbusAsynConfigure("portName", "tcpPortName", slaveAddress, modbusFunction, modbusStartAddress, modbusLength, dataType, pollMsec, "plcType")
-drvModbusAsynConfigure("INT32_BE", "sim1", 0, 3, 60, 20, 6, 100, "Simulator")
+drvModbusAsynConfigure("INT32_BE_F3", "sim1", 0, 3, 60, 20, 6, 100, "Simulator")
+drvModbusAsynConfigure("INT32_BE_F16","sim1", 0,16, 60, 20, 6, 100, "Simulator")
 
 
 # Enable ASYN_TRACEIO_HEX on octet server
