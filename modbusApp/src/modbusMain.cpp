729eb823e819ddbb345b77af7b7cd0a7392d9aac
Mark Rivers <rivers@cars.uchicago.edu>
2017-09-02 18:06:33 -0500

    New version of *Main.cpp file from base 3.15.5; contains epicsExit() after iocsh(NULL) which is needed for epicsAtExit() to work on Windows

--- modbusApp/src/modbusMain.cpp
+++ modbusApp/src/modbusMain.cpp
@@ -1,4 +1,4 @@
-/* modbusMain.cpp */
+/* _APPNAME_Main.cpp */
 /* Author:  Marty Kraimer Date:    17MAR2000 */
 
 #include <stddef.h>
@@ -7,15 +7,17 @@
 #include <string.h>
 #include <stdio.h>
 
+#include "epicsExit.h"
 #include "epicsThread.h"
 #include "iocsh.h"
 
 int main(int argc,char *argv[])
 {
-    if(argc>=2) { 
+    if(argc>=2) {    
         iocsh(argv[1]);
         epicsThreadSleep(.2);
     }
     iocsh(NULL);
+    epicsExit(0);
     return(0);
 }
